e4c086c705122cff4c11800dad6d2d294b49b330
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2023-08-15 08:27:02 +0500

    Update tiers selection
    Added the staking tier selection so that the user can get the specific tier rewards.

--- src/components/RewardClaimCard.jsx
+++ src/components/RewardClaimCard.jsx
@@ -1,11 +1,20 @@
-import React,{useState} from 'react';
-import { Card, CardContent, Typography, Select,InputLabel ,MenuItem, Button, FormControl } from '@mui/material';
+import React, { useState } from 'react';
+import {
+  Card,
+  CardContent,
+  Typography,
+  Select,
+  InputLabel,
+  MenuItem,
+  Button,
+  FormControl,
+} from '@mui/material';
 import { useMediaQuery, useTheme } from '@mui/material';
-import { transferAndConvertTokens } from '../Web3Helper'
+import { transferAndConvertTokens } from '../Web3Helper';
 
 const headerStyle = {
   textAlign: 'center',
-  margin:0,
+  margin: 0,
 };
 
 const contentStyle = {
@@ -16,80 +25,109 @@ const contentStyle = {
 };
 
 const dropdownStyle = {
-  //padding:0,
   width: '100%',
   marginBottom: '16px',
-  backgroundColor:"#0a570a",
-  //height:'40px',
-  color:'white'
+  backgroundColor: '#0a570a',
+  color: 'white',
 };
 
 const claimButtonStyle = {
-	backgroundColor:"#0a570a",
-
+  backgroundColor: '#0a570a',
   width: '100%',
 };
 
-const RewardClaimCard = ({web3,address,contract,rewardAmount,stakingContract}) => {
-	const theme = useTheme();
-  	const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-  	const cardWidth = isMobile ? '70%' : '50%';
-    const [claimWay,setClaimWay] =useState('')
-  	const cardStyle = {
-  		width: cardWidth,
-      
-  		margin: 'auto',
-  		backgroundColor: 'rgba(0,0,0,0.5)',
-  		color: 'white',
-	};
-const LabelStyle = {
-  transform: 'none', // Disables the upward animation
-  transition: 'none', // Disables any transition effects
-  marginLeft: '8px',
-  fontSize:'12px',
-  color:'white',
-  //remove the lable when the user selects an optio
-  // Add any other custom styles for the label here
-};
-  function handleChange(event){
-    setClaimWay(event.target.value)
-}
-function handleClick(){
-  
-  if(claimWay!== ''){
-    if(claimWay === 'fee_reward'){
-      transferAndConvertTokens(web3,address,contract,rewardAmount,stakingContract);
-    }else if(claimWay === 'stake_reward'){
-      stakingContract.methods._RewardWithdrawal(0).send({from:address});
-      console.log("stake rewards")
+const RewardClaimCard = ({ web3, address, contract, rewardAmount, stakingContract }) => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const cardWidth = isMobile ? '70%' : '50%';
+  const [claimWay, setClaimWay] = useState('');
+  const [selectedTier, setSelectedTier] = useState(''); // Added state for selected tier
+
+  const cardStyle = {
+    width: cardWidth,
+    margin: 'auto',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    color: 'white',
+  };
+
+  const LabelStyle = {
+    transform: 'none',
+    transition: 'none',
+    marginLeft: '8px',
+    fontSize: '12px',
+    color: 'white',
+  };
+
+  function handleChange(event) {
+    setClaimWay(event.target.value);
+    setSelectedTier(''); // Reset selected tier when changing claim way
+  }
+
+  function handleTierChange(event) {
+    setSelectedTier(event.target.value);
+  }
+
+  function handleClick() {
+    if (claimWay !== '') {
+      if (claimWay === 'fee_reward') {
+        transferAndConvertTokens(web3, address, contract, rewardAmount, stakingContract);
+      } else if (claimWay === 'stake_reward') {
+        if (selectedTier === 'tier1') {
+          stakingContract.methods._RewardWithdrawal(0).send({from:address});
+        } else if (selectedTier === 'tier2') {
+          stakingContract.methods._RewardWithdrawal(1).send({from:address});
+        } else if (selectedTier === 'tier3') {
+          stakingContract.methods._RewardWithdrawal(2).send({from:address});
+        } else {
+          alert("Please select a tier and try again!")
+          console.log('No tier selected');
+        }
+      }
     }
   }
-}
+
   return (
     <Card style={cardStyle} variant="outlined">
       <CardContent>
-        <h4 style={headerStyle}>
-          Select Reward
-        </h4>
+        <h4 style={headerStyle}>Select Reward</h4>
         <div style={contentStyle}>
-        <FormControl fullWidth>
-        {claimWay==='' && 
-        <InputLabel style={LabelStyle} id="reward-label" >Select reward type</InputLabel>
-        }
-        <Select
-          value={claimWay}
-          label="Select reward type"
-          onChange={handleChange}
-          style={dropdownStyle}
-          variant="filled"
-          color='inherit'
-        >
-          
-          <MenuItem value={'fee_reward'}>  Fee Reward   </MenuItem>
-          <MenuItem value={'stake_reward'}>Stake Reward</MenuItem>
-          
-        </Select>
-      </FormControl>
+          <FormControl fullWidth>
+            {claimWay === '' && (
+              <InputLabel style={LabelStyle} id="reward-label">
+                Select reward type
+              </InputLabel>
+            )}
+            <Select
+              value={claimWay}
+              label="Select reward type"
+              onChange={handleChange}
+              style={dropdownStyle}
+              variant="filled"
+              color="inherit"
+            >
+              <MenuItem value={'fee_reward'}>Fee Reward</MenuItem>
+              <MenuItem value={'stake_reward'}>Stake Reward</MenuItem>
+            </Select>
+          </FormControl>
+          {claimWay === 'stake_reward' && (
+            <FormControl fullWidth>
+              <InputLabel style={LabelStyle} id="tier-label">
+                Select tier
+              </InputLabel>
+              <Select
+                value={selectedTier}
+                label="Select tier"
+                onChange={handleTierChange}
+                style={dropdownStyle}
+                variant="filled"
+                color="inherit"
+              >
+                <MenuItem value={'tier1'}>Tier 1</MenuItem>
+                <MenuItem value={'tier2'}>Tier 2</MenuItem>
+                <MenuItem value={'tier3'}>Tier 3</MenuItem>
+              </Select>
+            </FormControl>
+          )}
           <Button variant="contained" style={claimButtonStyle} onClick={handleClick}>
             <span>Claim</span>
           </Button>
