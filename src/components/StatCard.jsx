357a7525fa074b38e2ceee1789ea3c41eab4c787
Muhammad Auzair <78294230+Nobody12here@users.noreply.github.com>
2023-08-17 10:05:51 +0500

    updated smart contract and UI

--- src/components/StatCard.jsx
+++ src/components/StatCard.jsx
@@ -2,25 +2,26 @@ import React from 'react';
 import { Card, CardContent } from '@mui/material';
 import { useMediaQuery, useTheme } from '@mui/material';
 
-const StatCard = ({ tokens, totalRewards, unclaimedRewards,GEtokensPrice,BTTPrice }) => {
+const StatCard = ({ tokens, totalRewards, unclaimedRewards, GEtokensPrice, BTTPrice }) => {
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
-  const cardWidth = isMobile ? '70%' : '50%';
-  
+  const cardWidth = isMobile ? '90%' : '50%';
+  const flexDirection = isMobile ? 'row' : 'row';
+
   return (
-    <Card variant="outlined" style={{ width: cardWidth,margin:'auto',height:'100%',marginTop:'1.5em',marginBottom:'1.5em', backgroundColor: 'rgba(0,0,0,0.5)', color: 'white' }}>
+    <Card variant="outlined" style={{ width: cardWidth, margin: 'auto', height: '100%', marginTop: '1.5em', marginBottom: '1.5em', backgroundColor: 'rgba(0,0,0,0.5)', color: 'white' }}>
       <CardContent>
-        <div style={{display: 'flex', justifyContent: 'space-between',fontSize:'12px'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-between', flexDirection: flexDirection, fontSize: '12px' }}>
           <div>
-          <h3>User Tokens:</h3>
-          <p>Total Rewards:</p>
-          <p>Unclaimed Rewards:</p>
+            <h3>User Tokens:</h3>
+            <h3>Total Rewards:</h3>
+            <h3>Unclaimed Rewards:</h3>
           </div>
           <div>
-          <h3>{tokens} GE ({(GEtokensPrice*tokens).toFixed(6)}$)</h3>
-          <p>{totalRewards} BTT ({(BTTPrice*totalRewards).toFixed(6)}$)</p>
-          <p>{unclaimedRewards}</p>
+            <h3>{tokens} GE ({(GEtokensPrice * tokens).toFixed(6)}$)</h3>
+            <h3>{totalRewards} BTT ({(BTTPrice * totalRewards).toFixed(6)}$)</h3>
+            <h3>{unclaimedRewards}</h3>
           </div>
         </div>
       </CardContent>
